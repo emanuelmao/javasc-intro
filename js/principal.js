bf3c81d5e495acb5557c2503238cbbee9add7dcc
emanuelmao <emanuel.engcomp@gmail.com>
2021-01-12 15:26:02 -0400

    querySelection; calculo do imc; selecionar td

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,23 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = "Aparecida Nutricionista";
+
+var tdPeso = document.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = document.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = document.querySelector(".info-imc");
+
+
+console.log(tdPeso);
+console.log(peso);
+
+console.log(tdAltura);
+console.log(altura);
+
+var imc = peso / (altura*altura);
+
+console.log(imc);
+
+tdImc.textContent = imc;
